46e4d35de74ed81034846339475ac8eb9a4b1875
Nick <tangerinefr0g@hotmail.com>
2026-06-17 22:02:59 +0700

    add emoji picker

--- web/src/lib/components/ChatInput.svelte
+++ web/src/lib/components/ChatInput.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-    import { KEYBOARD_KEYS } from '$lib/constants';
-    import paperclipIcon from '$lib/assets/icons/paperclip.svg?url';
+    import { KEYBOARD_KEYS } from "$lib/constants";
+    import paperclipIcon from "$lib/assets/icons/paperclip.svg?url";
+    import emojiIcon from "$lib/assets/icons/emoji.svg?url";
+    import { onMount } from "svelte";
 
-    export let value = '';
+    export let value = "";
     export let disabled = false;
     export let attachedFile: File | null = null;
     export let onSend: (file?: File | null) => void;
 
     let inputRef: HTMLInputElement;
     let fileInputRef: HTMLInputElement;
+    let showEmojiPicker = false;
 
     export function focusInput() {
         inputRef?.focus();
@@ -29,10 +32,27 @@
     function clearAttachment() {
         attachedFile = null;
         if (fileInputRef) {
-            fileInputRef.value = '';
+            fileInputRef.value = "";
         }
     }
+
+    function handleEmojiClick(event: any) {
+        value += event.detail.unicode;
+        inputRef?.focus();
+    }
+
+    function handlePickerClickOutside(event: any) {
+        if (!event.target.closest(".emoji-picker-wrapper")) {
+            showEmojiPicker = false;
+        }
+    }
+
+    onMount(async () => {
+        await import("emoji-picker-element");
+    });
 </script>
+
+<svelte:window on:click={handlePickerClickOutside} />
 
 <div class="chat-input">
     <label class="attachment-button" aria-label="Attach file">
@@ -41,33 +61,66 @@
             type="file"
             hidden
             on:change={handleFileChange}
-            disabled={disabled}
+            {disabled}
         />
         <img src={paperclipIcon} alt="Attach file" />
     </label>
 
     <div class="input-wrapper">
-        <input
-            bind:this={inputRef}
-            type="text"
-            placeholder="Type a message..."
-            bind:value={value}
-            on:keydown={handleKeydown}
-            aria-label="Message input"
-            disabled={disabled}
-        />
+        <div class="input-row">
+            <div class="input-with-emoji">
+                <input
+                    bind:this={inputRef}
+                    type="text"
+                    placeholder="Type a message..."
+                    bind:value
+                    on:keydown={handleKeydown}
+                    aria-label="Message input"
+                    {disabled}
+                />
+                <div class="emoji-picker-wrapper">
+                    <button
+                        type="button"
+                        class="emoji-button"
+                        on:click|stopPropagation={() =>
+                            (showEmojiPicker = !showEmojiPicker)}
+                        aria-label="Open emoji picker"
+                        {disabled}
+                    >
+                        <img src={emojiIcon} alt="Emoji" />
+                    </button>
+                    {#if showEmojiPicker}
+                        <div
+                            class="emoji-picker-popover"
+                            on:click|stopPropagation
+                        >
+                            <emoji-picker
+                                class="dark"
+                                on:emoji-click={handleEmojiClick}
+                            />
+                        </div>
+                    {/if}
+                </div>
+            </div>
+        </div>
 
         {#if attachedFile}
             <div class="attachment-preview">
                 <span>{attachedFile.name}</span>
-                <button type="button" on:click={clearAttachment} aria-label="Remove attachment">
-                    ×
-                </button>
+                <button
+                    type="button"
+                    on:click={clearAttachment}
+                    aria-label="Remove attachment">×</button
+                >
             </div>
         {/if}
     </div>
 
-    <button on:click={() => onSend(attachedFile)} aria-label="Send message" disabled={disabled || (!value.trim() && !attachedFile)}>
+    <button
+        on:click={() => onSend(attachedFile)}
+        aria-label="Send message"
+        disabled={disabled || (!value.trim() && !attachedFile)}
+    >
         Send
     </button>
 </div>
